09b9ca7b0259d1e274dff203194b79771b075fe2
andrew ahn <andrewsrahn@gmail.com>
2018-02-09 04:11:51 -0800

    i am so bad at making icons

--- src/App.js
+++ src/App.js
@@ -1,21 +1,93 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import settings from './settings.png'
+import minimize from './minimize.png'
+import close from './close.png'
+import league from './league.png'
+
+//electron react plumbing///////////////////
+const electron = window.require('electron')
+// const fs = electron.remote.require('fs')
+const ipcRenderer  = electron.ipcRenderer
+////////////////////////////////////////////
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div class='app'>
+				<div id='controls'>
+					<input class='control' type='image' src={league}/>
+					<input class='control' type='image' src={settings}/>
+					<input class='control' type='image' src={minimize}/>
+					<input class='control' type='image' src={close}/>
+					
+				</div>
+				<div class='column'>
+					<DisplayBans data='bans'/>
+
+					<Mate data='Duckiee'/>
+					<Mate data='RiotRazer'/>
+					<Mate data='MistGunner'/>
+					<Mate data='IanLezinski'/>
+					<Mate data='TimMcdaniel'/>
+				</div>
+
+				<div class='column' id='dynamic-display'>
+					<p>dynamic display</p>
+				</div>
+
+				<div class='column'>
+					<DisplayBans data='bans'/>
+					<Opponent data='sejuani'/>
+					<Opponent data='caitlyn'/>
+					<Opponent data='maokai'/>
+					<Opponent data='lee sin'/>
+					<Opponent data='what the fuck'/>
+				</div>
+
+
+			</div>
+		);
+	}
+
+	componentDidMount(){
+		ipcRenderer.on('client', (event, client) => {
+			this.setState( previousState => {
+				return {client: client}
+			})
+		})
+	}
+}
+
+
+class Mate extends Component {
+	render(){
+		return (
+			<div class='mate'>
+				<p>{this.props.data}</p>
+			</div>
+		)
+	}
+}
+
+class Opponent extends Component{
+	render(){
+		return (
+			<div class='opponent'>
+				<p>{this.props.data}</p>
+			</div>
+		)
+	}
+}
+
+class DisplayBans extends Component{
+	render(){
+		return(
+			<div id='display-bans'>
+				<p>display bans</p>
+			</div>
+		)
+	}
 }
 
 export default App;
