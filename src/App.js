46b3f6b8a5b440cb7628c0ad8f3d686e8a45ad84
andrew ahn <andrewsrahn@gmail.com>
2018-02-11 09:41:53 -0800

    flexyy

--- src/App.js
+++ src/App.js
@@ -5,57 +5,42 @@ import minimize from './minimize.png'
 import close from './close.png'
 import league from './league.png'
 
-//electron react plumbing///////////////////
-const electron = window.require('electron')
-// const fs = electron.remote.require('fs')
-const ipcRenderer  = electron.ipcRenderer
-////////////////////////////////////////////
+// christian sepulveda hack
+const electron = window.require('electron');
+const fs = electron.remote.require('fs');
+const ipcRenderer  = electron.ipcRenderer;
+// end hack
+
+
 
 class App extends Component {
 	render() {
 		return (
-			<div class='app'>
-				<div id='controls'>
-					<input class='control' type='image' src={league}/>
-					<input class='control' type='image' src={settings}/>
-					<input class='control' type='image' src={minimize}/>
-					<input class='control' type='image' src={close}/>
-					
+			<div id='app'>
+				<div id='left'>
+					<DisplayBans className='component' data='bans'/>
+					<Mate className='component' data='Duckiee'/>
+					<Mate className='component' data='Scarlett'/>
+					<Mate className='component' data='Destiny'/>
+					<Mate className='component' data='Smix'/>
+					<Mate className='component' data='Megumi'/>
 				</div>
-				<div class='column'>
-					<DisplayBans data='bans'/>
-
-					<Mate data='Duckiee'/>
-					<Mate data='RiotRazer'/>
-					<Mate data='MistGunner'/>
-					<Mate data='IanLezinski'/>
-					<Mate data='TimMcdaniel'/>
+				<div id='center'>
+					<input className='control' type='image' src={league}/>
+					<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 				</div>
-
-				<div class='column' id='dynamic-display'>
-					<p>dynamic display</p>
+				<div id='right'>
+					<input className='control' type='image' src={settings}/>
+					<input className='control' type='image' src={minimize}/>
+					<input className='control' type='image' src={close}/>
+					<Opponent className='component' data='Warwick'/>
+					<Opponent className='component' data='Jarvan 4'/>
+					<Opponent className='component' data='Sejuani'/>
+					<Opponent className='component' data='Leona'/>
+					<Opponent className='component' data='Janna'/>
 				</div>
-
-				<div class='column'>
-					<DisplayBans data='bans'/>
-					<Opponent data='sejuani'/>
-					<Opponent data='caitlyn'/>
-					<Opponent data='maokai'/>
-					<Opponent data='lee sin'/>
-					<Opponent data='what the fuck'/>
-				</div>
-
-
 			</div>
 		);
-	}
-
-	componentDidMount(){
-		ipcRenderer.on('client', (event, client) => {
-			this.setState( previousState => {
-				return {client: client}
-			})
-		})
 	}
 }
 
@@ -63,7 +48,7 @@ class App extends Component {
 class Mate extends Component {
 	render(){
 		return (
-			<div class='mate'>
+			<div className='mate'>
 				<p>{this.props.data}</p>
 			</div>
 		)
@@ -73,7 +58,7 @@ class Mate extends Component {
 class Opponent extends Component{
 	render(){
 		return (
-			<div class='opponent'>
+			<div className='opponent'>
 				<p>{this.props.data}</p>
 			</div>
 		)
